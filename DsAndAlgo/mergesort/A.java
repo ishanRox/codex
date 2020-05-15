e76b053da8349a11b264acf3f72c27c5825624ff
ishanRox <roxishan8@gmail.com>
2020-05-15 06:00:04 +0530

    initiated data strructures and algorithems

---
+++ DsAndAlgo/mergesort/A.java
@@ -0,0 +1,23 @@
+
+class A{
+
+private static void merge(int left[],int right[],int main[]) {
+    int leftIndex=0,rightIndex=0,mainIndex=0;
+
+    while (leftIndex<left.length&&rightIndex<right.length) {
+        
+        if (left[leftIndex]<right[rightIndex]) {
+            main[mainIndex]=left[leftIndex];
+
+        }else{
+
+        }
+    }
+
+}
+    public static void main(String[] args) {
+        System.out.println("ishan okksfdsfkk");
+    
+    }
+}
+
