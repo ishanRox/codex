5936d1b93b37dfabc471c9d2382e6d8e83290572
ishanRox <roxishan8@gmail.com>
2020-05-15 10:12:00 +0530

    mergesort recursive part added and completed

--- DsAndAlgo/mergesort/A.java
+++ DsAndAlgo/mergesort/A.java
@@ -39,27 +39,50 @@ class A {
     }
 
     private static void sort(int array[]) {
-        
-        if (array.length<2) {
+
+        if (array.length < 2) {
             return;
         }
 
-        int mid=array.length/2;
-        int left[]=new int[mid];
-        int right[]=new int[]
+        int mid = array.length / 2;
+        int left[] = new int[mid];
+        int right[] = new int[array.length - mid];
+
+        for (int i = 0; i < mid; i++) {
+
+            left[i] = array[i];
+        }
+
+        for (int i = mid; i < array.length; i++) {
+
+            right[i - mid] = array[i];
+        }
+
+        sort(left);
+        sort(right);
+
+        merge(left, right, array);
     }
 
     public static void main(String[] args) {
-       
+
         int left[] = IntStream.range(0, 10).toArray();
         int right[] = IntStream.range(10, 20).toArray();
         int main[] = new int[20];
 
-        merge(left, right, main);
+        // merge(left, right, main);
 
-        //yse merging of two arrays worked
-        Arrays.stream(main).forEach(System.out::println);
-    
-    
+        // // yse merging of two arrays worked
+        // Arrays.stream(main).forEach(System.out::print);
+
+        // System.out.println();
+        // //check dividing of arrays working
+        // sort(main);
+
+
+        int []arrayToSort=new int[]{4,3,2,1,-12,13};
+        sort(arrayToSort);
+
+        System.out.println(Arrays.stream(arrayToSort).boxed().collect(Collectors.toList()));
     }
 }
