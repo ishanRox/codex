348c49f09c6e962651f00341d551151ce9a9da26
ishanRox <roxishan8@gmail.com>
2020-06-03 06:52:02 +0530

    combine futures and thenCompose

--- java some what difficult theory/compatibleFuture/3/A_Z.java
+++ java some what difficult theory/compatibleFuture/3/A_Z.java
@@ -19,6 +19,7 @@ public class A_Z {
         } catch (Exception e) {
             // TODO: handle exception
         }
+        System.out.println();
         // ________________________________________________________________________________________________________________________________________
 
         // runAsync() for processes that didnt return
@@ -33,6 +34,8 @@ public class A_Z {
             System.out.println("I'll run in a separate thread than the main thread and this thread is daemon ."
                     + Thread.currentThread());
         });
+
+        System.out.println();
         // ________________________________________________________________________________________________________________________________________
 
         // run some task and return a result
@@ -63,6 +66,8 @@ public class A_Z {
         // this may run in that (pending ) but if its resolved
         // this use calling thread
         future1.thenAccept(e -> System.out.println("result recived " + e + "   " + Thread.currentThread()));
+
+        System.out.println();
         // ________________________________________________________________________________________________________________________________________
         // thenApply and thenApplyAsync wala wenasa thama
         /*
@@ -70,15 +75,82 @@ public class A_Z {
          * executed or in the main thread If the supplyAsync() task completes
          * immediately (Remove sleep() call to verify)
          *
-         * But thenApplyAsync Executed in a different thread from 
-         * ForkJoinPool.commonPool() Executed in
-         * a different thread from ForkJoinPool.commonPool()
+         * But thenApplyAsync Executed in a different thread from
+         * ForkJoinPool.commonPool() Executed in a different thread from
+         * ForkJoinPool.commonPool()
          */
+        CompletableFuture.supplyAsync(() -> {
+            return "Some Result";
+        }).thenApplyAsync(result -> {
+            // Executed in a different thread from ForkJoinPool.commonPool()
+            System.out.println(Thread.currentThread() + " this is a different thread defineately in fork join");
+            return "Processed Result";
+        }).thenAcceptAsync(e -> System.out.println(e + " see it uses different forkjoins " + Thread.currentThread()));
+
+        System.out.println();
+        // ________________________________________________________________________________________________________________________________________
+        // ________________________________________________________________________________________________________________________________________
+        // combine completable futures and play with multiple of them
+        CompletableFuture<String> usersDetail = getUsersDetail("#123");
+
+        // api userDetail gen awa future eka modifi karanawa eke thiyena detai aran
+        // thawa future ekakata eee data yawanawidihata
+        // so ehema unahama nested future seen eka enawa data eka ganna nam future 2
+        // resolve karanna one
+        CompletableFuture<CompletableFuture<String>> completableFuture1 = usersDetail
+                .thenApply(user -> getCreditRating(user));
+
+        // when we accept the result we ger completableFuture which contain
+        // completableFuture<String> this is more work.
+        // so insted of this we use thenCompose which directly give output which given
+        // by completableFuture<String>.
+        completableFuture1
+                .thenAccept(completableFuture2 -> completableFuture2.thenAccept(string -> System.out.println(string)));
+
+        // because of this nesting completableFuture problem we use thenCompose
+        // apita nested ewala final future eka auto resolve karala denawa thenCompose
+        CompletableFuture<String> stringCompletableFuture = usersDetail.thenCompose(user -> getCreditRating(user));
+
+        // ___________________________________________________________________________________________________________________
+        // method 3 kata balamu completableFuture data ethakota nested futures 3k enawa
+        CompletableFuture<CompletableFuture<CompletableFuture<String>>> threeNestedFutures = usersDetail
+                .thenApply(user -> getCreditRating(user).thenApply(credit -> getAnotherDetail(credit)));
+
+        // but then compose use kalama wade lesi nested wechcha ewalin final eka aran
+        // denawa
+        CompletableFuture<String> finalOf3Nested = usersDetail
+                .thenCompose(user -> getCreditRating(user).thenCompose(credit -> getAnotherDetail(credit)));
+        finalOf3Nested.thenAccept(finalString -> System.out.println(finalString));
+        System.out.println();
         try {
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.SECONDS.sleep(12);
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
         }
         System.out.println("mainend");
+    }
+
+    // combining 2 completable Futures
+    static CompletableFuture<String> getUsersDetail(String userId) {
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println("method 1 finished");
+            return ", data from get user detail - " + userId;
+        });
+    }
+
+    static CompletableFuture<String> getCreditRating(String user) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            System.out.println("method 2 finished");
+            return ", data from get credit detail  12,0000 " + user;
+        });
+    }
+
+    static CompletableFuture<String> getAnotherDetail(String user) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            System.out.println("method 3 finished");
+            return " data from another which use credit card data " + user;
+        });
     }
 }
