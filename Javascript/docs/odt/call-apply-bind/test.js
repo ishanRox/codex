2c0d31be87fea56b684d395cc979681d4980fef7
ishanRox <roxishan8@gmail.com>
2020-08-04 10:57:06 +0530

    call apply this bind not work with arrow funcitons

--- Javascript/docs/odt/call-apply-bind/test.js
+++ Javascript/docs/odt/call-apply-bind/test.js
@@ -1,4 +1,4 @@
-
+//call start
 const calculateAge = function () {
     console.log(this.age);
     console.log(this.name);
@@ -11,3 +11,26 @@ const ishan = {
 };
 
 calculateAge.call(ishan);
+
+
+//apply method started
+let manyArgs = function (name, age, sex) {
+    console.log(this);
+    console.log(`full name of client person is ${this.fullName}`);
+    console.log(`name ${name} age ${age} sex ${sex}`);
+
+};
+
+const obj = { fullName: "ishan vimukthi kandage don", codeNo: 6656 };
+
+console.log(obj);
+manyArgs.apply(obj, ["drone", 24, "male"]);
+
+
+//is call and apply work for arrow funcitons ? 
+
+const m1 = () => console.log(`hello  ${this.name}`);
+
+m1.call({ name: "ishan" });
+//sadly no because of arrow functions get their this form lexical scope
+
